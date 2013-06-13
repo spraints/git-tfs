703bb2334bc16a0eac953f3a74c2a0976a05bd39
Matt Burke <spraints@gmail.com>
2013-06-13 19:21:25 -0400

    Pausing profiler, for external profilers.

---
+++ GitTfs/Profiling/PausingProfiler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using StructureMap;
+using System.Diagnostics;
+
+namespace GitTfs.Profiling
+{
+    /// <summary>
+    /// Let you use an external sampling profiler.
+    /// </summary>
+    [Pluggable("external")]
+    public class PausingProfiler : Profiler
+    {
+        public override void Sample(string sampleName)
+        {
+            Console.WriteLine("Paused PID " + Process.GetCurrentProcess().Id + " for profiling. Press <Enter> to continue.");
+            Console.ReadLine();
+        }
+    }
+}
