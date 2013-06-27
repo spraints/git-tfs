2629ea78076681cbadfa8f9865530e178e74fcb5
Matt Burke <spraints@gmail.com>
2013-06-27 09:13:10 -0400

    Stub checkin tests.

---
+++ GitTfsTest/Integration/CheckinTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xunit;
+
+namespace Sep.Git.Tfs.Test.Integration
+{
+    public class CheckinTests : IDisposable
+    {
+        IntegrationHelper h;
+
+        public CheckinTests()
+        {
+            h = new IntegrationHelper();
+        }
+
+        public void Dispose()
+        {
+            h.Dispose();
+        }
+
+        [FactExceptOnUnix]
+        public void ChecksIn()
+        {
+            throw new NotImplementedException("todo: test for `git tfs checkin`");
+        }
+
+        [FactExceptOnUnix]
+        public void DoesAGatedCheckin()
+        {
+            throw new NotImplementedException("todo: test for `git tfs checkin` when gated checkins are required on the project");
+        }
+    }
+}
